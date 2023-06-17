513a10c8e2c5a83e02c13e9d80483083638af194
Yuto Takakura <yutotakakura0320@gmail.com>
2023-06-17 18:05:55 +0900

    Add another answer of 347

--- solutions/347.top-k-frequent-elements.py
+++ solutions/347.top-k-frequent-elements.py
@@ -10,10 +10,37 @@ import heapq
 # @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        My answer using heapq
+        O(n)
+        """
         count_num = Counter(nums)
         data = [(-count_num[num], num) for num in count_num]
         heapq.heapify(data)
         return [heapq.heappop(data)[1] for _ in range(k)]
+
+        """
+        Other answer
+        O(n)
+        """
+        """
+        count = {}
+
+        # Index is count of nums, so range is len(nums) + 1
+        freq = [[] for _ in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        """
         
 # @lc code=end
 
