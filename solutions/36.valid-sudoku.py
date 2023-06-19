e7ae83972909201ceab3560427589af29727f757
Yuto Takakura <yutotakakura0320@gmail.com>
2023-06-20 08:02:43 +0900

    [add]36

---
+++ solutions/36.valid-sudoku.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+from collections import defaultdict
+from typing import List
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
+        
+# @lc code=end
+
